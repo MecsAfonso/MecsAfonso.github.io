43310c426488121749b440e41b2495ca06152839
Maximiliano Afonso <mecsafonso@gmail.com>
2021-02-09 22:52:47 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
+    
+    <Router>
+   
+
+   <div className="App">
+      
       <h1> Hello World</h1>
+      <Link to="/portafolio"> <h5> Portafolio </h5> </Link>
+
     </div>
+
+  </Router>
+
+   
   );
 }
 
